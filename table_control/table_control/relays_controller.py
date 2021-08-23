c8e371cce99b72f9b423fae97e806c76802f8e31
AdamVondracek <Adam_Vondracek@protonmail.com>
2021-08-23 11:04:05 +0200

    Relays control change proposal

--- table_control/table_control/relays_controller.py
+++ table_control/table_control/relays_controller.py
@@ -6,9 +6,7 @@ except ModuleNotFoundError:
     class Relays:
         def Set(self, value):
             pass
-        
-        def Stop(self):
-            pass
+
     
 class RelaysControl:
     """
@@ -19,80 +17,74 @@ class RelaysControl:
     ----------
     relays : rpi_hardware.Relays
         register shifter for relay controll
-    
-    relaysValue : int
-        binary number, representing array of bools
         
-    avaiableRelays : tuple
+    _avaiableRelays : tuple
         tuple containing names of relays
         
-    relaysNames : dictionary
+    _relaysNames : dictionary
         keys = avaiableRelays, values = index(avaiableRelays)
         hash map for quick lookup
-    
     """
     def __init__(self):
         logging.debug("RelaysControl - __init__")
         self.relays = Relays()  
-        self.relaysValue = 0
-        self.avaiableRelays = ("PcSwitch","none_1","PcPower","PcComponentsPower","none_4","none_5","none_6","LedsPower")
-        self.relaysNames = { name : i for i,name in enumerate(self.avaiableRelays)}
+        self._avaiableRelays = ("PcSwitch","none_1","PcPower","PcComponentsPower","none_4","none_5","none_6","LedsPower")
+        self._relaysStatus = { name : 0 for name in self._avaiableRelays}
 
-        # todo uncoment
-        # self.TurnOfAll()
-
-    def SetAll(self, value) -> None:
+    def _update(self) -> None:
         """
-        Sets relays according to 8 bit number
+        Updates relays from _relaysStatus
         """
-        logging.debug(f"RelaysControl - SetAll {bin(value)}")
-        self.relaysValue = value
+        logging.debug(f"RelaysControl - update {self._relaysStatus}")
+        
+        value = self.ToBin()
         self.relays.Set(value)
-
-    def SetSome(self, values) -> None:
-        """
-        values is a list of 8-bit numbers, calls Relays.Set from rpi_hardware
-        """
-        logging.debug(f"RelaysControl - SetSome {values}")
-        value = self.relaysValue
-        for val in values:
-            if val in self.relaysNames:
-                value ^= ((value >> self.relaysNames[val] & 1) ^ values[val]) << self.relaysNames[val]
-            else:
-                logging.error(f"RelaysControl - SetSome ERROR - value: {val} is not in relaysNames")
-
-        self.relaysValue = value
-        self.relays.Set(value)
-
-    def StartPc(self):
-        """
-        sequence of operations that safely poweres PC
-        """
-        logging.debug("RelaysControl - StartPc")
-        value = self.relaysValue
-        # todo 
-        self.relays.Set(value)
-
-    def TurnOfAll(self) -> None:
-        """
-        Sets relaysValue to 0x00 (base-16) and calls Relays.Stop() from rpi_hardware
-        """
-        logging.debug("RelaysControl - TurnOfAll")
-        self.relaysValue = 0x00
-        self.relays.Stop()
 
     def GetStatus(self) -> dict:
         """
-        Return dictionary, keys = relaysNames, values = bool
+        Return dictionary, keys = _relaysNames, values = bool
         
-        Converts relaysValue (8-bit) to dictonary, keys = relaysNames and values = bool representing if relay works
+        bool values represent if relay is on/off
         """
         logging.debug("RelaysControl - GetStatus")
-        relaysStatus = dict()
-        for relay in self.relaysNames:
-            relaysStatus[relay] = (self.relaysValue >> self.relaysNames[relay]) & 1
-        logging.debug(f"RelaysControl - status is: {relaysStatus}")
-        return relaysStatus
+        logging.debug(f"RelaysControl - status is: {self._relaysStatus}")
+        return self._relaysStatus.copy()
+    
+    def FromStatus(self,new_status: dict = dict()) -> None:
+        """
+        Sets self._relaysStatus to new_status
+        """
+        logging.debug("RelaysControl - FromStatus")
+        for relay in new_status:
+            if relay in self._avaiableRelays:
+                self._relaysStatus[relay] = new_status[relay]
+        self._update()
+                
+    def FromBin(self,binary_number: int = int()) -> None:
+        """
+        Sets self._relaysStatus values from binary number
+        """
+        logging.debug("RelaysControl - FromBin")
+        bool_arr = [False] * 8
+        
+        for i in range(8):
+            bool_arr[i] = binary_number & 1
+            binary_number = binary_number >> 1
+                
+        self._relaysStatus = dict(zip(self._avaiableRelays,bool_arr))
+        self._update()
+    
+    def ToBin(self) -> int:
+        """
+        Converts _relaysStatus to binary number
+        """
+        logging.debug("RelaysControl - ToBin")
+        value = 0x0
+        for relay in self._avaiableRelays[::-1]:
+            value = (value + self._relaysStatus[relay]) << 1
+        value = value >> 1
+        logging.debug(f"RelaysControl - binary value is {bin(value)}")
+        return value
 
 def _test_relays_controller():
     def await_user_input():
@@ -103,20 +95,17 @@ def _test_relays_controller():
     relay_controller.GetStatus()
     
     logging.debug("Turn on all")
-    relay_controller.SetAll(0b11111111)
-    relay_controller.GetStatus()
+    relay_controller.FromBin(0xff)
     await_user_input()
     
     logging.debug("Turn off all") 
-    relay_controller.TurnOfAll()
-    relay_controller.GetStatus()
+    relay_controller.FromBin(0x00)
     await_user_input()
     
     logging.debug("Turn on individualy")
     x = 0x1
-    for i in range(8):
-        relay_controller.SetAll(x)
-        relay_controller.GetStatus()
+    for _ in range(8):
+        relay_controller.FromBin(x)
         x = x << 1
         await_user_input()
         
@@ -124,8 +113,7 @@ def _test_relays_controller():
     x = 0x0
     for _ in range(8):
         x = (x << 1) + 0x1
-        relay_controller.SetAll(x)
-        relay_controller.GetStatus()
+        relay_controller.FromBin(x)
         await_user_input()
     
     
